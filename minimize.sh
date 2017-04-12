91f8168064955be93243f188eef6dd5cb4ba6bc1
Mischa ter Smitten <mischa@tersmitten.nl>
2017-04-12 11:26:30 +0200

    Shellcheck fixes

--- minimize.sh
+++ minimize.sh
@@ -11,21 +11,21 @@ resize2fs "${rootvolume}";
 
 # Zero out free space on /
 count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
-count=$(($count - 1))
+count=$((count - 1))
 whitespacefile='/whitespace';
 dd if=/dev/zero of="${whitespacefile}" bs=1M count="${count}" || echo "dd exit code $? is suppressed";
 rm "${whitespacefile}";
 
 # Zero out free space on /boot
 count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}');
-count=$(($count - 1));
+count=$((count - 1));
 whitespacefile='/boot/whitespace';
 dd if=/dev/zero of="${whitespacefile}" bs=1M count="${count}" || echo "dd exit code $? is suppressed";
 rm "${whitespacefile}";
 
 swapfile='/swap';
 count=$(stat -c '%s' "${swapfile}");
-count=$(($count / 1024));
+count=$((count / 1024));
 swapoff "${swapfile}";
 dd if=/dev/zero of="${swapfile}" bs=1024 count="${count}" || echo "dd exit code $? is suppressed";
 mkswap "${swapfile}";
